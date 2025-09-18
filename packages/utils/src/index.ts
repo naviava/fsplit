cb85addd0eb27c2e196631a5fe2c76ff8b1c4b7b
naviava <knavinavadhani@gmail.com>
2025-09-18 06:27:56 +0000

    chore: Added utils package

---
+++ packages/utils/src/index.ts
@@ -0,0 +1,33 @@
+import { TRPCError } from "@trpc/server";
+
+type Success<T> = {
+  data: T;
+  error: null;
+};
+type Failure<E> = {
+  data: null;
+  error: E;
+};
+type Result<T, E = Error> = Success<T> | Failure<E>;
+export async function tryCatch<T, E = Error>(
+  promise: Promise<T>
+): Promise<Result<T, E>> {
+  try {
+    const data = await promise;
+    return { data, error: null };
+  } catch (error) {
+    return { data: null, error: error as E };
+  }
+}
+
+export function throwInternalServerError(
+  error: unknown | null,
+  message: string
+): never {
+  if (error !== null) console.error(message + ": " + error);
+  throw new TRPCError({
+    code: "INTERNAL_SERVER_ERROR",
+    message,
+    cause: error || undefined,
+  });
+}
