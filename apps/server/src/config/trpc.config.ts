a4602935ae4140aac7adba7b31a36241a04bc0d4
naviava <knavinavadhani@gmail.com>
2025-09-26 11:06:14 +0000

    chore: Added user and session to tRPC context

--- apps/server/src/config/trpc.config.ts
+++ apps/server/src/config/trpc.config.ts
@@ -1,31 +1,18 @@
 import type { CreateExpressContextOptions } from "@trpc/server/adapters/express";
-// import jwt from "jsonwebtoken";
 
-// import type { TUserContext } from "@fsplit/types";
 import { db } from "@fsplit/db";
-import Env from "../lib/env";
+import { auth } from "../lib/auth";
 
-export function createContext({ req, res }: CreateExpressContextOptions) {
-  const authHeader = req.headers.authorization;
-  const token = authHeader?.split(" ")[1];
-  const unauthenticated = {
+export async function createContext({ req, res }: CreateExpressContextOptions) {
+  const session = await auth.api.getSession({
+    headers: req.headers as unknown as Headers,
+  });
+  return {
     req,
     res,
-    user: null,
     db,
+    user: session?.user ?? null,
+    session: session?.session ?? null,
   };
-  if (!token) return unauthenticated;
-  try {
-    // const user = jwt.verify(token, Env.JWT_SECRET as string) as TUserContext;
-    const user = "USER_TOKEN";
-    return {
-      req,
-      res,
-      user,
-      db,
-    };
-  } catch (err) {
-    return unauthenticated;
-  }
 }
 export type Context = Awaited<ReturnType<typeof createContext>>;
